d0689c37e249382eb83466e63fbd80da2d1d58bd
halchester <chester.htooaunglinn@gmail.com>
2021-07-10 19:24:26 +0630

    feat: basic use case

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
-import { DB } from './db';
+import { DB } from "./db";
 
 const data = [
-	{
-		id: 1,
-		data: {
-			name: 'asdf',
-		},
-	},
-	{
-		id: 2,
-		data: {
-			name: 'asdfasdf',
-		},
-	},
+  {
+    id: 1,
+    data: {
+      name: "Chester",
+      age: 18,
+    },
+  },
+  {
+    id: 2,
+    data: {
+      name: "Another person",
+      age: 21,
+    },
+  },
 ];
 
 const myDB = new DB(data);
 
-console.log(myDB.getAll());
-console.log(myDB.getById(1));
+// Getting all data
+myDB.getAll();
+
+// Getting data by id
+myDB.getById(1);
+
+// Removing data
+myDB.remove(1);
+
+// Edit data
+myDB.update(2, { name: "Another Another person", age: 21 });
+
+// Adding data
+myDB.add({ id: 3, data: { name: "Another next person", age: 12 } });
