78da65dc72fcb454f8250c126909c00ea7d4e0d7
Chester <chester.htooaunglinn@gmail.com>
2021-07-15 19:28:57 +0630

    feat: remove unnecessary codes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 import { generateDB } from "./factory";
-import { data } from "./sampledata";
 
 export default generateDB;
-
-// Use case
-interface dataSchema {
-  name: string;
-  email: string;
-  age: number;
-}
-
-const myDB = generateDB<dataSchema>(data);
